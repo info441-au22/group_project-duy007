ba9ed234d4ef7ddfbd79f9c471e4884bf8f5a2c9
Duy Nguyen <duyhoangnguyen2000@gmail.com>
2022-12-06 22:59:04 -0800

    deployment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const appSettings = {
         clientSecret:  "ucV8Q~DdVenOGoAWLJTrRz7oAxXmJ-t20sejobpP"
     },	
     authRoutes: {
-        redirect: "http://localhost:3000/redirect", //note: you can explicitly make this "localhost:3000/redirect" or "examplesite.me/redirect"
+        redirect: "https://studyroomfinder.dhnguyen.me/redirect", //note: you can explicitly make this "localhost:3000/redirect" or "examplesite.me/redirect"
         error: "/error", // the wrapper will redirect to this route in case of any error.
         unauthorized: "/unauthorized" // the wrapper will redirect to this route in case of unauthorized access attempt.
     }
